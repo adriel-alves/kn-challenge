6f851c8eef61581b46fd0204d14a52895091633f
Adriel Alves <adrielfreitasalves@gmail.com>
2023-01-18 15:49:10 -0300

    configuration for path and lazy-loading

--- kn-challenge/src/app/app-routing.module.ts
+++ kn-challenge/src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', pathMatch:'full', redirectTo:'contact-list'}
+  {path: '', pathMatch:'full', redirectTo:'contact-list'},
+  {
+    path: 'contact-list',
+    loadChildren: () => import('./contact-list/contact-list.module').then(m => m.ContactListModule)
+  }
 ];
 
 @NgModule({
